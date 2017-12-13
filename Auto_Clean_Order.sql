21d8b7b0d2b64bdd12a505ef0a15e4922dde1bcb
Xia Zhao <xiazhao@redhat.com>
2017-12-13 13:48:38 +0800

    Update Auto_Clean_Order.sql

--- Auto_Clean_Order.sql
+++ Auto_Clean_Order.sql
@@ -1,21 +1,12 @@
-SELECT DISTINCT snapshot.damage_id
-from as_order as_order, inspection_damage damage,inspection_damage_snapshot_id  snapshot
-where as_order.id=damage.order_id and snapshot.damage_id=damage.id and as_order.sa_id=17
-DELETE FROM inspection_damage_snapshot_id WHERE damage_id IN ()
+DELETE snapshot,damage FROM inspection_damage_snapshot_id snapshot, as_order as_order, inspection_damage damage where as_order.id=damage.order_id and snapshot.damage_id=damage.id and as_order.sa_id=17;
 
-select DISTINCT item.pre_inspection_info_id
-from as_order as_order, inspection_base_info base_info,inspection_item  item
-where as_order.id=base_info.order_id and base_info.id=item.pre_inspection_info_id and as_order.sa_id=17
+DELETE item,base_info from inspection_item item,as_order as_order, inspection_base_info base_info
+  where as_order.id=base_info.order_id and item.pre_inspection_info_id=base_info.id and as_order.sa_id=17;
 
-DELETE from inspection_item WHERE pre_inspection_info_id IN ()
+DELETE FROM inspection_base_info WHERE EXISTS(SELECT 1 FROM as_order WHERE as_order.id=inspection_base_info.order_id AND as_order.sa_id=17);
 
-
-DELETE FROM inspection_base_info WHERE EXISTS(SELECT 1 FROM as_order WHERE as_order.id=inspection_base_info.order_id AND as_order.sa_id=17)
+DELETE kdm_details,cart_item FROM shopping_cart_item cart_item, as_order as_order, shopping_cart_item_kdm_details kdm_details
+where as_order.id=cart_item.order_id and cart_item.id=kdm_details.item_id and as_order.sa_id=17;
 
 
-DELETE FROM inspection_damage WHERE EXISTS(SELECT 1 FROM as_order WHERE as_order.id=inspection_damage.order_id AND as_order.sa_id=17)
-
-DELETE FROM shopping_cart_item WHERE EXISTS(SELECT 1 FROM as_order WHERE as_order.id=shopping_cart_item.order_id AND as_order.sa_id=17)
-
-
-DELETE  FROM as_order WHERE as_order.sa_id=17
+DELETE  FROM as_order WHERE as_order.sa_id=17;
